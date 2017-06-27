129bd148edcbf45c2d419780ae2e0b42e2860b1d
Jonathan Dequeker <jonathan.dequeker@gmail.com>
2017-06-27 21:05:45 +0200

    Reworked stylesheet
    alle css opnieuw geschreven om de foto’s in zo veel mogelijk gevallen
    binnen het scherm te laten tonen zonder dat er gescrolt moet worden.

--- js/app/projects.js
+++ js/app/projects.js
@@ -28,7 +28,7 @@ angular.module('sw')
     'folieadeux': {
       name:'folieadeux',
       order: 1701,
-      title: 'Folie à Deux, Gent',
+      title: 'Folie à Deux',
       description:'Koffiehuis in Gent, 2017.',
       thumb: 'folieadeux0.jpg',
       pictures: [
@@ -84,13 +84,17 @@ angular.module('sw')
         {
           src: 'still3.jpg',
           description: ''
+        },
+        {
+          src: '',
+          description: '<p>Still is an ever-changing image. The tubular structure seems to be fleeting from the viewer’s cognitive understanding. By walking around the object, the image changes as the underlying structure dimly appears.<br>So complex the structure, so simple is the tabletop; a solid piece of hardwood with the right dimensions and the right height.</p>'
         }
       ]
     },
     'gloriette': {
       name:'gloriette',
       order: 1509,
-      title: 'Gloriette, Heuvelland',
+      title: 'Gloriette',
       description:'Tuinomgeving in Poperinge, 2015.',
       thumb: 'gloriette0.jpg',
       pictures: [
@@ -125,6 +129,10 @@ angular.module('sw')
         {
           src: 'gloriette8.jpg',
           description: ''
+        },
+        {
+          src: '',
+          description: '<p>Uitgestrekte velden over het glooiende heuvelland brachten een scherpe wind uit het noordwesten.<br>Een poreuze structuur vangt ze op en wendt ze af: enkel de hoogste takken getuigen nog van de harde wind.</p><p>Herfst, winter, lente, zomer: de akkers dragen een kleurrijke garderobe.<br>Glas, als stilstaand water, vertroebelt het landschapskleed tot vlakken van kleur. Een filter om bij het opkijken in de velden veel meer te zien.</p><p>Een crescendo voor het oog, valt de blik van links naar rechts de verte in naar de stad en haar hoogste torens.<br>Een barrière van hardhouten latten golft speels met het landschap mee, eerst hoog, dan geleidelijk lager. Om de kijker het deinzen van de ogen aan te leren en ze daarna te belonen.</p><p>Oude stenen werpen onbekommerd schaduw over verleden en toekomst.<br>Water weerkaatst nu een heldere hemel. Het is een oninneembare kalmte die de dorre dromen klaart.<br>Een ruimte los en overgaand, tussen weidse wereld en veilig binnen.</p><p>Een verdiepingshoge structuur biedt steun aan het wolkendak, omlijst muren van tuin, met vensters tussen wilgenbomen.<br>Een <em>gloriëtte:</em> om geborgen buiten te vertoeven, een anker voor het huis, een uitnodiging om tussen natuur te tafelen.</p>'
         }
       ]
 
